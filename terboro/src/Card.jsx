afaf738950231ea26778604544a70a0c65aa8ae1
Terboro <112891254+robertorincos@users.noreply.github.com>
2025-12-17 12:31:31 +0000

    Card adição do texto e adição do card no main

---
+++ terboro/src/Card.jsx
@@ -0,0 +1,15 @@
+import './Card.css'
+
+function Card() {
+
+  return (
+    <>
+      <div className='carta'>
+        <h2>Terboro</h2>
+        <p>Yapper supremo da desinformação</p>
+      </div>
+    </>
+  )
+}
+
+export default Card
