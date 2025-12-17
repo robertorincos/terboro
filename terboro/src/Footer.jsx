deb6bb54b9f4f62c85f29497e26d2acb340ec8eb
Terboro <112891254+robertorincos@users.noreply.github.com>
2025-12-17 14:46:50 +0000

    adição da imagem de rodapé WII

--- terboro/src/Footer.jsx
+++ terboro/src/Footer.jsx
@@ -1,10 +1,12 @@
 import './Footer.css'
+import imgFooter from './assets/footer-removebg-preview.png'
 
 function Footer() {
 
   return (
     <>
       <div className='pesao'>
+        <img src={imgFooter} className="barraFooter" alt="wii footer" />
       </div>
     </>
   )
