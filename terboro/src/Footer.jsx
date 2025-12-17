69d9a384e6ac716ad238b0b8b13ce443f26c1297
Terboro <112891254+robertorincos@users.noreply.github.com>
2025-12-17 13:45:24 +0000

    Adição de footer

---
+++ terboro/src/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+function Footer() {
+
+  return (
+    <>
+      <div className='pesao'>
+      </div>
+    </>
+  )
+}
+
+export default Footer
--- terboro/src/main.jsx
+++ terboro/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Card from './Card.jsx'
+import Footer from './Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
     <Card />
+    <Footer />
   </StrictMode>,
 )
