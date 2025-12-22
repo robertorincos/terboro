3a2e47ff371fc39284dc5167e83451aaee463861
Terboro <112891254+robertorincos@users.noreply.github.com>
2025-12-22 12:51:03 +0000

    aprimoramento da responsividade do footer

--- terboro/src/Footer.jsx
+++ terboro/src/Footer.jsx
@@ -1,5 +1,4 @@
 import './Footer.css'
-import imgFooter from './assets/footer-removebg-preview.png'
 import Button from './Button.jsx'
 import { useSound } from './context/SoundContext'
 
@@ -7,22 +6,34 @@ function Footer() {
   const { isMuted, toggleMute } = useSound();
 
   return (
-    <footer className='pesao'>
-      <div className='footer-buttons'>
+    <footer className='footer-container'>
+      <div className='footer-left'>
         <Button className='button-left' onClick={toggleMute}>
-          {isMuted ? (
-            <svg width="50%" height="50%" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                <path d="M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73 4.27 3zM12 4L9.91 6.09 12 8.18V4z"/>
-            </svg>
-          ) : (
-            <svg width="50%" height="50%" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                <path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"/>
-            </svg>
-          )}
+          <div className="button-content">
+            {isMuted ? (
+              <svg width="72" height="72" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73 4.27 3zM12 4L9.91 6.09 12 8.18V4z"/>
+              </svg>
+            ) : (
+              <svg width="72" height="72" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"/>
+              </svg>
+            )}
+          </div>
         </Button>
-        <Button className='button-right' />
       </div>
-      <img src={imgFooter} className="barraFooter" alt="wii footer" />
+      
+      <div className='footer-middle'>
+         <span className="footer-description">Footer description goes here</span>
+      </div>
+
+      <div className='footer-right'>
+        <Button className='button-right'>
+            <div className="button-content">
+              <span>Ask</span>
+            </div>
+        </Button>
+      </div>
     </footer>
   )
 }
